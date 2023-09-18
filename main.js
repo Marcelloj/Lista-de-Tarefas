94babe4e9901941dc4f07c4b08783e17b0ca1d8d
unknown <MarcelloJr>
2023-09-18 20:38:34 -0300

    inicialização do projeto com arquivos basicos html css js

---
+++ main.js
@@ -0,0 +1,18 @@
+function addNewTask() {
+    var list = document.getElementById("list");
+       var text = document.getElementById("task_name").value;
+    
+    if (text.length === 0) {
+        alert("Tarefa precisa de ter mais de 1 caracter");
+        return
+    }
+    
+    
+    var listItem = document.createElement("li");
+    listItem.className = "list-item";
+    
+    const textElement = document.createTextNode(text);
+    listItem.appendChild(textElement);
+    list.appendChild(listItem);
+    
+    }
